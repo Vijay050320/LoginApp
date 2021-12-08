7be33f929d110d66a6ecd49d9d6e318e44b5d22d
vijay050320 <scmvijaykumark@gmail.com>
2021-12-08 19:08:38 +0530

    added

--- src/main/java/com/my/app/App.java
+++ src/main/java/com/my/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-         System.out.println( "Hello Devops!" );
+         System.out.println( "Hello" );
     }
 }
